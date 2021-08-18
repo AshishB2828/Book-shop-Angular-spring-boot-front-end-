7bef541ed917a11a425eb4d6420f36e73b94770b
Ashish <unnib2828@gmail.com>
2021-08-18 11:40:09 +0530

    added image to home component

--- src/app/models/Book.model.ts
+++ src/app/models/Book.model.ts
@@ -2,6 +2,7 @@ export class Book{
     id:number|undefined;
     title:string="";
     description:string="";
+    imageUrl:string="";
     author:string="";
     price:number=0;
     createTime:Date=new Date();
